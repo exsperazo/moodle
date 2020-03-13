412e79175fdf8d0c4ea922b00503161c83f112b7
exsperazo <magumbamark@hotmail.com>
2020-03-13 16:08:28 +0300

    Update config.php
    settings

--- config.php
+++ config.php
@@ -6,14 +6,14 @@ $CFG = new stdClass();
 
 $CFG->dbtype    = 'pgsql';
 $CFG->dblibrary = 'native';
-$CFG->dbhost    = 'postgres://hfqgecaffghnbh:08baccd35c9f427c86c211ab1a75bfcd3692f2dd7d2a39439cd01352247aa300@ec2-54-152-175-141.compute-1.amazonaws.com:5432/dfu1m6169uqjlh';
-$CFG->dbname    = getenv('DATABASE_NAME');
-$CFG->dbuser    = getenv('DATABASE_USER');
-$CFG->dbpass    = getenv('DATABASE_PASSWORD');
+$CFG->dbhost    = 'ec2-54-152-175-141.compute-1.amazonaws.com';
+$CFG->dbname    = 'dfu1m6169uqjlh';
+$CFG->dbuser    = 'hfqgecaffghnbh';
+$CFG->dbpass    = '08baccd35c9f427c86c211ab1a75bfcd3692f2dd7d2a39439cd01352247aa300';
 $CFG->prefix    = 'mdl_';
 $CFG->dboptions = array (
   'dbpersist' => 0,
-  'dbport' => getenv('DATABASE_PORT'),
+  'dbport' => 5432,
   'dbsocket' => '',
 );
 
